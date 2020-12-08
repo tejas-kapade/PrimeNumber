d9238743d3c391e4fea3f8cc9b8fc44ee997ef51
Tejas Kapade <75604489+tejas-kapade@users.noreply.github.com>
2020-12-08 22:51:48 +0530

    PrimeDetector
    Used "elif" method to check multiple possibilities.

--- PrimeNumber.py
+++ PrimeNumber.py
@@ -2,21 +2,18 @@
 
 prime = True  #useful to set a boolean for state of prime number
 
-num = int(input("Enter a number")) #take user input
+num = int(input("Enter a number___")) #take user input
 
 if( (num > 1) and (num < 9000) ):
   for a in range(2,num) :
     if(num % a) == 0:
-       print("Not A Prime number")
+       print(num,"is Not A Prime number")
        prime = False
   if(prime): #if prime is true then this will be executed
-       print("Number is prime")
+       print(num," is a Prime Number")
 
 elif(num == 1):  #elif used to make more possibilitys
   print("1 is Not a Prime Number")
-
-elif(num <= 0):
-  print("Number must be positive")
 
 elif(num > 8999): 
   print("Number must lower than 4 digits , because programme may crash to calculate larger nums" )
